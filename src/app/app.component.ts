fe0cd39a3af3a8e119b1d66d31f67ba243daae93
unknown <bddkhoa@gmail.com>
2023-12-07 23:05:52 +0700

    add components (navbar and page contents)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PageContentsComponent } from './components/page-contents/page-contents.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,NavbarComponent,PageContentsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
