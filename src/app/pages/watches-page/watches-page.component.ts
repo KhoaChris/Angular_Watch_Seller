31d8c9136c7a3272586c5e29963c127e10c9e5f1
unknown <bddkhoa@gmail.com>
2023-12-16 00:08:43 +0700

    done page watches showcase

--- src/app/pages/watches-page/watches-page.component.ts
+++ src/app/pages/watches-page/watches-page.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { PageContentsWatchesComponent } from '../../components/page-contents-watches/page-contents-watches.component';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { BottomRowNavbarComponent } from '../../components/bottom-row-navbar/bottom-row-navbar.component';
 
 @Component({
   selector: 'app-watches-page',
   standalone: true,
-  imports: [CommonModule, PageContentsWatchesComponent, NavbarComponent],
+  imports: [CommonModule, PageContentsWatchesComponent, NavbarComponent, BottomRowNavbarComponent],
   templateUrl: './watches-page.component.html',
   styleUrl: './watches-page.component.scss',
   changeDetection: ChangeDetectionStrategy.Default,
