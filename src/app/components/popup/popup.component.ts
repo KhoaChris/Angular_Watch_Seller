db4ac3cff83cfb93c790bdab82bb3fbbc0fc7bb2
unknown <bddkhoa@gmail.com>
2024-01-02 00:02:14 +0700

    add svg

--- src/app/components/popup/popup.component.ts
+++ src/app/components/popup/popup.component.ts
@@ -1,5 +1,11 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Inject, ViewEncapsulation } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Inject,
+  PLATFORM_ID,
+  ViewEncapsulation,
+} from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
@@ -9,13 +15,27 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
   templateUrl: './popup.component.html',
   styleUrl: './popup.component.scss',
   changeDetection: ChangeDetectionStrategy.Default,
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class PopupComponent {
   constructor(
     public dialogRef: MatDialogRef<PopupComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    @Inject(PLATFORM_ID) private platformId: Object
   ) {}
+
+  isLogin = false;
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const storedData = window.localStorage.getItem('isLogin');
+      if (storedData) {
+        this.isLogin = true;
+      }
+    } else {
+      console.log('You must active in the browser');
+    }
+  }
 
   close(): void {
     this.dialogRef.close();
