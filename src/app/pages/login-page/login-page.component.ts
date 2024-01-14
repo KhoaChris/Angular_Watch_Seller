82ae610b52b0bdc0e32f31e6d519356bf31ffdd8
Nguyen Tricsie <bddkhoa@gmail.com>
2024-01-14 16:42:53 +0700

    fix UI of login page

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -3,6 +3,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { PageContentsLoginComponent } from '../../components/page-contents-login/page-contents-login.component';
 import { SignupPageComponent } from '../signup-page/signup-page.component';
+import { BottomRowNavbarComponent } from '../../components/bottom-row-navbar/bottom-row-navbar.component';
 
 @Component({
   selector: 'app-login-page',
@@ -12,6 +13,7 @@ import { SignupPageComponent } from '../signup-page/signup-page.component';
     NavbarComponent,
     PageContentsLoginComponent,
     SignupPageComponent,
+    BottomRowNavbarComponent
   ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss',
