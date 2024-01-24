13ab0aaae7ecc8c291b8d1d62156c0ccb23d0b3f
unknown <bddkhoa@gmail.com>
2024-01-24 21:03:32 +0700

    update UI of login and signup

--- src/app/pages/signup-page/signup-page.component.ts
+++ src/app/pages/signup-page/signup-page.component.ts
@@ -2,11 +2,17 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { PageContentsSignupComponent } from '../../components/page-contents-signup/page-contents-signup.component';
+import { BottomRowNavbarComponent } from '../../components/bottom-row-navbar/bottom-row-navbar.component';
 
 @Component({
   selector: 'app-signup-page',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, PageContentsSignupComponent],
+  imports: [
+    CommonModule,
+    NavbarComponent,
+    PageContentsSignupComponent,
+    BottomRowNavbarComponent,
+  ],
   templateUrl: './signup-page.component.html',
   styleUrl: './signup-page.component.scss',
   changeDetection: ChangeDetectionStrategy.Default,
