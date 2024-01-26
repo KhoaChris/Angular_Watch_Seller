c1a3a7bd9faf613bccc412d5c48216ce3caead38
unknown <bddkhoa@gmail.com>
2024-01-27 00:47:11 +0700

    update page contact us

--- src/app/pages/contact-page/contact-page.component.ts
+++ src/app/pages/contact-page/contact-page.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { PageContentsContactComponent } from '../../components/page-contents-contact/page-contents-contact.component';
+import { BottomRowNavbarComponent } from '../../components/bottom-row-navbar/bottom-row-navbar.component';
 
 @Component({
   selector: 'app-contact-page',
@@ -9,7 +10,8 @@ import { PageContentsContactComponent } from '../../components/page-contents-con
   imports: [
     CommonModule,
     NavbarComponent,
-    PageContentsContactComponent
+    PageContentsContactComponent,
+    BottomRowNavbarComponent
   ],
   templateUrl: './contact-page.component.html',
   styleUrl: './contact-page.component.scss',
